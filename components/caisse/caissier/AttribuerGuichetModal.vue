b329e46a326a9e5cefd01550ff4d35db8479bf5b
Ahoussou Jean-Chris Arnaud <shin.ahoussou@gmail.com>
2022-11-15 11:42:23 +0000

    Détails caissier calendrier d'attribution des guichets

--- components/caisse/caissier/AttribuerGuichetModal.vue
+++ components/caisse/caissier/AttribuerGuichetModal.vue
@@ -139,7 +139,6 @@ export default {
   },
   mounted() {
     this.getSites()
-    console.log(this.caissier)
   },
   methods: {
     ...mapActions({
--- components/caisse/caissier/ShowCaissierModal.vue
+++ components/caisse/caissier/ShowCaissierModal.vue
@@ -1,9 +1,143 @@
 <template>
-  <div></div>
+  <b-modal v-model="dialog" size="xl" scrollable hide-footer>
+    <template #modal-header>
+      <h5 class="modal-title text-primary">Détails du caissier {{ caissier.user.name }}</h5>
+      <button type="button" class="close" aria-label="Close" @click="dialog = false">
+        <span aria-hidden="true"><feather type="x" /></span>
+      </button>
+    </template>
+    <template #default>
+      <v-app>
+        <v-expansion-panels v-model="panel" popout focusable tile>
+          <v-expansion-panel>
+            <v-expansion-panel-header
+              ><h6>Calendrier d'attribution des guichets</h6></v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <v-row class="fill-height">
+                <v-col>
+                  <v-sheet height="64">
+                    <v-toolbar flat>
+                      <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+                        Aujourd'hui
+                      </v-btn>
+                      <v-btn fab text small color="grey darken-2" @click="$refs.calendar.prev()">
+                        <v-icon small> mdi-chevron-left </v-icon>
+                      </v-btn>
+                      <v-btn fab text small color="grey darken-2" @click="$refs.calendar.next()">
+                        <v-icon small> mdi-chevron-right </v-icon>
+                      </v-btn>
+                      <v-toolbar-title v-if="$refs.calendar">
+                        {{ $refs.calendar.title }}
+                      </v-toolbar-title>
+                      <v-spacer></v-spacer>
+                    </v-toolbar>
+                  </v-sheet>
+                  <v-sheet height="700">
+                    <v-calendar
+                      ref="calendar"
+                      v-model="focus"
+                      locale="fr-FR"
+                      color="primary"
+                      :weekdays="weekday"
+                      :events="events"
+                      type="month"
+                      @click:event="deleteItem"
+                      @click:more="viewDay"
+                      @click:date="viewDay"
+                    ></v-calendar>
+                  </v-sheet>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+        <v-row justify="center">
+          <v-dialog v-model="dialogDelete" persistent max-width="450">
+            <v-card>
+              <v-card-title class="text-h6 error--text">Confirmation d'annulation ?</v-card-title>
+              <v-card-text>
+                Voulez-vous réelement supprimer l'attribution du guichet
+                {{ editedItem.name }} pour la date du
+                {{ $moment(editedItem.date).format('DD-MM-YYYY') }}
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="error darken-1" text @click="dialogDelete = false"> abandonner </v-btn>
+                <v-btn color="primary darken-1" text @click="remove"> accepter </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
+      </v-app>
+    </template>
+  </b-modal>
 </template>
 
 <script>
-export default {}
+import { mapActions } from 'vuex'
+export default {
+  props: {
+    caissier: {
+      type: Object,
+      required: true,
+    },
+    value: Boolean,
+  },
+  data: () => ({
+    focus: '',
+    weekday: [0, 1, 2, 3, 4, 5, 6],
+    panel: 0,
+    editedItem: {
+      name: null,
+      jour: null,
+    },
+    dialogDelete: false,
+  }),
+  computed: {
+    dialog: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      },
+    },
+    events() {
+      return this.caissier.attributions.map(({ id: guichet, code, pivot: { id, date } }) => {
+        return { id, guichet, start: date, end: date, name: code, timed: true }
+      })
+    },
+  },
+  methods: {
+    ...mapActions('caisse/caissier', ['desattribuer']),
+    deleteItem({ event }) {
+      this.editedItem = Object.assign({}, event)
+      this.dialogDelete = true
+    },
+    remove() {
+      this.desatribuer(this.editedItem.id).then(({ message }) => {
+        this.$bvToast.toast(message, {
+          title: "succès de l'annulation".toLocaleUpperCase(),
+          variant: 'success',
+          solid: true,
+        })
+        this.dialogDelete = false
+        this.dialog = false
+      })
+    },
+    viewDay({ date }) {
+      this.focus = date
+      this.type = 'day'
+    },
+    getEventColor(event) {
+      return event.color
+    },
+    setToday() {
+      this.focus = ''
+    },
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
