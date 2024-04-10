1d24336766e4cccbf1f5dd4b65bdd66e1602eb79
Ahoussou Jean-Chris Arnaud <shin.ahoussou@gmail.com>
2024-04-10 02:13:25 +0000

    nom des permissions changés chez le client

--- helper/permissions.js
+++ helper/permissions.js
@@ -1,123 +1,324 @@
-const parametre = {
-  acceder: 'accès au menu parametre',
-  configuration: {
-    acceder: 'accès à la configuration des sites'
-  },
-  utilisateur: {
-    acceder: 'accès aux utilisateurs et permissions'
-  },
-  gabaris: {
-    acceder: 'accès au gabaris de documents'
-  },
-  caisse: {
-    acceder: 'accès aux parametres de la caisse'
-  },
-  application: {
-    acceder: "accès aux parametres de l'application"
-  }
+const utilisateur = {
+  global: 'global-user',
+  own: 'own-user',
+  create: 'create-user',
+  trash: 'trash-user',
+  restore: 'restore-user',
+  delete: 'delete-user',
+  edit: 'edit-user',
+  show: 'show-user',
+  attribuateRole: 'attribuate-role-user',
 }
-const finance = {
-  acceder: 'accès au menu finance et quittancement',
-  facturation: {
-    acceder: 'accès au menu facturation'
-  },
-  bordereaux: {
-    acceder: 'accès au menu bordereaux',
-    commerciaux: {
-      acceder: 'accès aux commerciaux',
-      create: 'création des commerciaux',
-      edit: 'modifier un commercial',
-      trash: 'archiver un commercial',
-      restore: 'restorer un commercial',
-      show: "voir les détails d'un commercial"
-    },
-    bordereau: {
-      acceder: 'accès à la liste des bordereau',
-      show: "voir les détails d'un bordereau"
-    },
-    collecte: {
-      acceder: 'accès à la collecte des emplacements',
-      create: 'création de la collecte'
-    }
-  },
-  caisse: {
-    ouverture: {
-      acceder: "accès à l'ouverture de caisse",
-      create: "création d'ouverture de caisse",
-      trash: 'archiver une ouverture de caisse',
-      restore: 'restorer une ouverture de caisse'
-    },
-    point: {
-      acceder: 'accès au point de caisse'
-    },
-    encaissement: {
-      acceder: 'accès aux encaissements',
-      create: "création d'encaissement",
-      closable: 'fermer la caisse'
-    }
-  }
+const role = {
+  list: 'list-role',
+  create: 'create-role',
+  trash: 'trash-role',
+  restore: 'restore-role',
+  delete: 'delete-role',
+  edit: 'edit-role',
 }
-const exploitation = {
-  acceder: 'accès au menu exploitation',
-  reception: {
-    acceder: 'accès au menu réception',
-    demande: {
-      create: 'création de demande',
-      acceder: 'accès aux demandes',
-      edit: 'modifier une demande',
-      trash: 'archiver une demande',
-      restore: 'restorer une demande',
-      validate: 'valider une demande',
-      show: "voir les détails d'une demande"
-    },
-    prospect: {
-      create: 'création de prospect',
-      acceder: 'accès aux prospects',
-      edit: 'modifier un prospect',
-      trash: 'archiver un prospect',
-      restore: 'restorer un prospect',
-      show: "voir les détails d'un propspect"
-    },
-    client: {
-      acceder: 'accès aux clients',
-      edit: 'modifier un client',
-      trash: 'archiver un client',
-      restore: 'restorer un client',
-      show: "voir les détails d'un client"
-    },
-    contrat: {
-      acceder: 'accès aux contrats',
-      show: "voir les détails d'un contrat"
-    }
-  },
-  ordonnancements: {
-    acceder: 'accès au menu ordonnancements',
-    ordonnancement: {
-      create: "création d'ordonnancement",
-      show: "voir les détails d'un ordonnancement"
-    }
-  },
-  maintenance: {
-    acceder: 'accès au menu maintenance',
-    reparation: {
-      acceder: 'accès au menu réparation',
-      create: 'créer une réparation',
-      trash: 'archiver une réparation',
-      estimate: 'attaché des dévis de réparation',
-      show: "voir les détails d'une réparation",
-      validate: 'valider une réparation'
-    },
-    technicien: {
-      acceder: 'accès au menu technicien',
-      create: 'créer une technicien',
-      trash: 'archiver un technicien',
-      show: "voir les détails d'un technicien"
-    },
-    ordre: {
-      acceder: 'accès au menu ordre de réparation',
-      validate: 'valider un ordre de réparation'
-    }
-  }
+const permission = {
+  list: 'list-permission',
+  edit: 'edit-permission',
+}
+const configurationDashboard = {
+  structure: 'structure-site',
+}
+const site = {
+  global: 'global-site',
+  own: 'own-site',
+  create: 'create-site',
+  trash: 'trash-site',
+  delete: 'delete-site',
+  edit: 'edit-site',
+  show: 'show-site',
+}
+const pavillon = {
+  global: 'global-pavillon',
+  own: 'own-pavillon',
+  create: 'create-pavillon',
+  trash: 'trash-pavillon',
+  restore: 'restore-pavillon',
+  delete: 'delete-pavillon',
+  edit: 'edit-pavillon',
+}
+const niveau = {
+  global: 'global-niveau',
+  own: 'own-niveau',
+  create: 'create-niveau',
+  trash: 'trash-niveau',
+  restore: 'restore-niveau',
+  delete: 'delete-niveau',
+  edit: 'edit-niveau',
+}
+const zone = {
+  global: 'global-zone',
+  own: 'own-zone',
+  create: 'create-zone',
+  trash: 'trash-zone',
+  restore: 'restore-zone',
+  delete: 'delete-zone',
+  edit: 'edit-zone',
+}
+const emplacement = {
+  global: 'global-emplacement',
+  own: 'own-emplacement',
+  create: 'create-emplacement',
+  trash: 'trash-emplacement',
+  restore: 'restore-emplacement',
+  delete: 'delete-emplacement',
+  edit: 'edit-emplacement',
+  show: 'show-emplacement',
+}
+const equipement = {
+  global: 'global-equipement',
+  own: 'own-equipement',
+  create: 'create-equipement',
+  trash: 'trash-equipement',
+  delete: 'delete-equipement',
+  restore: 'restore-equipement',
+  edit: 'edit-equipement',
+  show: 'show-equipement',
+}
+const abonnement = {
+  global: 'global-abonnement',
+  ow: 'own-abonnement',
+  create: 'create-abonnement',
+  trash: 'trash-abonnement',
+  delete: 'delete-abonnement',
+  restore: 'restore-abonnement',
+  edit: 'edit-abonnement',
+  show: 'show-abonnement',
+  abort: 'abort-abonnement',
+}
+const typeEquipement = {
+  global: 'global-type-equipement',
+  own: 'own-type-equipement',
+  create: 'create-type-equipement',
+  restore: 'restore-type-equipement',
+  trash: 'trash-type-equipement',
+  delete: 'delete-type-equipement',
+  edit: 'edit-type-equipement',
 }
 
-export { parametre, finance, exploitation }
+const typeEmplacement = {
+  global: 'global-type-emplacement',
+  own: 'own-type-emplacement',
+  create: 'create-type-emplacement',
+  restore: 'restore-type-emplacement',
+  trash: 'trash-type-emplacement',
+  delete: 'delete-type-emplacement',
+  edit: 'edit-type-emplacement',
+}
+const typePersonne = {
+  global: 'global-type-personne',
+  own: 'own-type-personne',
+  create: 'create-type-personne',
+  restore: 'restore-type-personne',
+  trash: 'trash-type-personne',
+  delete: 'delete-type-personne',
+  edit: 'edit-type-personne',
+}
+const serviceAnnexe = {
+  global: 'global-annexe',
+  own: 'own-annexe',
+  create: 'create-annexe',
+  trash: 'trash-annexe',
+  restore: 'restore-annexe',
+  delete: 'delete-annexe',
+  edit: 'edit-annexe',
+  show: 'show-annexe',
+}
+const guichet = {
+  global: 'global-guichet',
+  own: 'own-guichet',
+  create: 'create-guichet',
+  restore: 'restore-guichet',
+  trash: 'trash-guichet',
+  delete: 'delete-guichet',
+  edit: 'edit-guichet',
+  show: 'show-guichet',
+}
+const caissier = {
+  global: 'global-caissier',
+  own: 'own-caissier',
+  create: 'create-caissier',
+  trash: 'trash-caissier',
+  restore: 'restore-caissier',
+  attribuate: 'attribuate-caissier',
+  show: 'show-caissier',
+}
+const compte = {
+  global: 'global-compte',
+  own: 'own-compte',
+  create: 'create-compte',
+  edit: 'edit-compte',
+}
+const banque = {
+  global: 'global-banque',
+  own: 'own-banque',
+  create: 'create-banque',
+  trash: 'trash-banque',
+  restore: 'restore-banque',
+  delete: 'delete-banque',
+  edit: 'edit-banque',
+}
+const application = {
+  list: 'list-application',
+  edit: 'edit-application',
+}
+const gabariContrat = {
+  global: 'global-gabari-contrat',
+  own: 'own-gabari-contrat',
+  create: 'create-gabari-contrat',
+  trash: 'trash-gabari-contrat',
+  delete: 'delete-gabari-contrat',
+  edit: 'edit-gabari-contrat',
+  show: 'show-gabari-contrat',
+}
+const receptionDashboard = {
+  list: 'list-reception-dashboard',
+  filter: 'filter-reception-dashboard',
+}
+const contrat = {
+  global: 'global-contrat',
+  own: 'own-contrat',
+  create: 'create-contrat',
+  trash: 'trash-contrat',
+  restore: 'restore-contrat',
+  delete: 'delete-contrat',
+  edit: 'edit-contrat',
+  schedule: 'schedule-contrat',
+  endorse: 'endorse-contrat',
+  validate: 'validate-contrat',
+  show: 'show-contrat',
+  listValidGlobal: 'global-contrat-valid',
+  listValidOwn: 'own-contrat-valid',
+  validShow: 'show-contrat-valid',
+}
+const personne = {
+  global: 'global-personne',
+  own: 'own-personne',
+  trash: 'trash-personne',
+  delete: 'delete-personne',
+  restore: 'restore-personne',
+  edit: 'edit-personne',
+  show: 'show-personne',
+}
+const ordonnancementDashboard = {
+  list: 'list-ordonnancement-dashboard',
+  filter: 'filter-ordonnancement-dashboard',
+}
+const ordonnancement = {
+  global: 'global-ordonnancement',
+  own: 'own-ordonnancement',
+  show: 'show-ordonnancement',
+}
+const factureDashboard = {
+  list: 'list-facture-dashboard',
+  filter: 'filter-facture-dashboard',
+}
+const factureEquipement = {
+  lobal: 'global-facture-equipement',
+  own: 'own-facture-equipement',
+  show: 'show-facture-equipement',
+}
+const factureAnnexe = {
+  global: 'global-facture-annexe',
+  own: 'own-facture-annexe',
+  show: 'show-facture-annexe',
+}
+const factureInitiale = {
+  global: 'global-facture-initiale',
+  own: 'own-facture-initiale',
+  show: 'show-facture-initiale',
+  edit: 'edit-facture-initiale',
+}
+const factureLoyer = {
+  global: 'global-facture-loyer',
+  own: 'own-facture-loyer',
+  show: 'show-facture-loyer',
+}
+const bordereauDashboard = {
+  list: 'list-bordereau-dashboard',
+  filter: 'filter-bordereau-dashboard',
+}
+const bordereau = {
+  global: 'global-bordereau',
+  own: 'own-bordereau',
+  collect: 'collect-bordereau',
+  edit: 'edit-bordereau',
+  show: 'show-bordereau',
+}
+const commercial = {
+  global: 'global-commercial',
+  own: 'own-commercial',
+  trash: 'trash-commercial',
+  restore: 'restore-commercial',
+  edit: 'edit-commercial',
+  show: 'show-commercial',
+  attribuate: 'attribuate-commercial',
+}
+const caisseDashboard = {
+  list: 'list-caissier-dashboard',
+  filter: 'filter-caissier-dashboard',
+}
+const ouverture = {
+  global: 'global-ouverture',
+  own: 'own-ouverture',
+  create: 'create-ouverture',
+  show: 'show-ouverture',
+}
+const fermeture = {
+  global: 'global-fermeture',
+  own: 'own-fermeture',
+  create: 'create-fermeture',
+  show: 'show-fermeture',
+}
+const encaissement = {
+  global: 'global-encaissement',
+  own: 'own-encaissement',
+  create: 'create-encaissement',
+  show: 'show-encaissement',
+}
+
+export {
+  encaissement,
+  fermeture,
+  ouverture,
+  role,
+  caisseDashboard,
+  commercial,
+  bordereau,
+  bordereauDashboard,
+  factureAnnexe,
+  factureEquipement,
+  factureInitiale,
+  factureLoyer,
+  factureDashboard,
+  ordonnancement,
+  ordonnancementDashboard,
+  application,
+  banque,
+  compte,
+  contrat,
+  gabariContrat,
+  personne,
+  receptionDashboard,
+  caissier,
+  guichet,
+  serviceAnnexe,
+  typeEmplacement,
+  typePersonne,
+  typeEquipement,
+  utilisateur,
+  permission,
+  configurationDashboard,
+  site,
+  pavillon,
+  niveau,
+  zone,
+  emplacement,
+  equipement,
+  abonnement,
+}
