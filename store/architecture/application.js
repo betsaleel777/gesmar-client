28a3120120a50fc1d4902f11d78153840bc58e92
betsaleel777 <shin.ahoussou@gmail.com>
2024-06-26 14:14:44 +0000

    correction de page de d'application (societe) pour le déploiement

--- store/architecture/application.js
+++ store/architecture/application.js
@@ -1,23 +1,18 @@
 export const state = () => ({
-  societe: {}
+  societe: {},
 })
 export const getters = {
-  societe: (state) => {
-    return state.societe
-  }
+  societe: (state) => state.societe,
 }
 export const actions = {
   async getOne({ commit }) {
     commit('SET_SOCIETE', [])
-    const requete = await this.$axios.get('api/parametres/societes/')
+    const requete = await this.$axios.get('api/parametres/societes')
     commit('SET_SOCIETE', requete.data.societe)
   },
 
   async modifier({ dispatch }, payload) {
-    const requete = await this.$axios.post(
-      'api/parametres/societes/' + payload.get('id') + '?_method=PUT',
-      payload
-    )
+    const requete = await this.$axios.post('api/parametres/societes/' + payload.get('id') + '?_method=PUT', payload)
     dispatch('getOne')
     return { message: requete.data.message }
   },
@@ -26,11 +21,11 @@ export const actions = {
     const requete = await this.$axios.post('api/parametres/societes/store', payload)
     dispatch('getOne')
     return { message: requete.data.message }
-  }
+  },
 }
 
 export const mutations = {
   SET_SOCIETE(state, societe) {
     state.societe = societe
-  }
+  },
 }
